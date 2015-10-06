900304409b9e7e63d8f8d2c2226fc23293050705
comfybunny <yanxialin2001@yahoo.com>
2015-10-06 04:06:27 -0400

    update
    P1 complete calling it for the night

--- HW2-Yan-Xialin/Q3/Q3Script.py
+++ HW2-Yan-Xialin/Q3/Q3Script.py
@@ -10,33 +10,24 @@ jsonfile = open('afc.json', 'w')
 reader = csv.DictReader(csvfile)
 jsonfile.write('{"nodes":[')
 
-derp = {}
 
-data = {}
-
+temp_list = []
 for derpy in reader:
-	# print(derp[derpy['NAME']]+"\n")
-	derp[derpy['NAME']] = int(derpy['APPEARANCES']);
-	# print(str(derp[derpy['NAME']])+"\n")
+    temp_dict = {}
+    temp_dict['name'] = derpy['NAME']
+    temp_dict['position'] = derpy['POSITION']
+    temp_dict['appearances'] = int(derpy['APPEARANCES'])
+    temp_dict['goals'] = int(derpy['GOALS'])
+    temp_dict['START_SEASON'] = int(derpy['START_SEASON'])
+    temp_dict['END_SEASON'] = int(derpy['END_SEASON'])
+    temp_list.append(temp_dict)
 
-sorted_derp = sorted(derp.items(), key=operator.itemgetter(1,0))
+sorted_list = sorted(temp_list, key=lambda t: (-t['appearances'], t['name'].lower()))
+sorted_list = sorted_list[:50]
 
-sigh = sorted_derp[-50:]
+for i in range(0,50):
+	jsonfile.write('\n{"name":"'+sorted_list[i]['name']+'","position":"'+sorted_list[i]['position']+'","appearances":'+str(sorted_list[i]['appearances'])+',"goals":'+str(sorted_list[i]['goals'])+'},')
 
-# print(sigh)
-
-derpsighugh = {}
-
-for iterate in sigh:
-	derpsighugh[iterate[0]] = iterate[1]
-
-readerd = csv.DictReader(q)
-
-for i in readerd:
-	if i['NAME'] in derpsighugh:
-		jsonfile.write('\n')
-		json.dump(i, jsonfile)
-		jsonfile.write(',')
 jsonfile.close()
 
 with open('afc.json', 'rb+') as filehandle:
@@ -46,8 +37,6 @@ with open('afc.json', 'rb+') as filehandle:
 with open('afc.json', 'a') as filehandle:
     filehandle.write(']}')
 
-with open('afc.json') as data_file:    
-    data = json.load(data_file)
 
 # print(data['nodes'][0]['START_SEASON'])
 with open('afc.json', 'rb+') as filehandle:
@@ -58,11 +47,11 @@ with open('afc.json', 'a') as filehandle:
     filehandle.write(',' + '\n' +'"links":[')
     for x in range (0, 50):
 		for y in range(x, 50):
-			if(x!=y and data['nodes'][x]['START_SEASON'] <= data['nodes'][y]['END_SEASON'] and data['nodes'][y]['START_SEASON'] <= data['nodes'][x]['END_SEASON']):
+			if(x!=y and sorted_list[x]['START_SEASON'] <= sorted_list[y]['END_SEASON'] and sorted_list[y]['START_SEASON'] <= sorted_list[x]['END_SEASON']):
 				# print(data['nodes'][x]['START_SEASON'] + " " + data['nodes'][x]['END_SEASON'] + "\n");
 				# print(data['nodes'][y]['START_SEASON'] + " " + data['nodes'][y]['END_SEASON'] + "\n");
-				startDate = int(max(data['nodes'][x]['START_SEASON'], data['nodes'][y]['START_SEASON']));
-				endDate = int(min(data['nodes'][x]['END_SEASON'], data['nodes'][y]['END_SEASON']));
+				startDate = int(max(sorted_list[x]['START_SEASON'], sorted_list[y]['START_SEASON']));
+				endDate = int(min(sorted_list[x]['END_SEASON'], sorted_list[y]['END_SEASON']));
 				tempValue = endDate-startDate+1
 				# print(str(startDate) + " " + str(endDate) + " " + str(tempValue) + "\n")
 				filehandle.write('\n{"source":' + str(x) + ',"target":' + str(y) + ',"value":' + str(tempValue) + '},')
